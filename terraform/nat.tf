4da704bd710f5bf2ce169c2965c335a588a7f1f8
ghtaidt9 <ghtaidt9@github.com>
2026-08-05 08:15:31 +0700

    Create IGW, NAT and Route table

---
+++ terraform/nat.tf
@@ -0,0 +1,31 @@
+###############################################
+# Elastic IP for NAT Gateway
+###############################################
+
+resource "aws_eip" "nat" {
+
+    domain = "vpc"
+
+    tags = {
+        Name = "${local.name_prefix}-nat-eip"
+    }
+
+    depends_on = [aws_internet_gateway.main]
+}
+
+###############################################
+# NAT Gateway
+###############################################
+
+resource "aws_nat_gateway" "main" {
+
+    allocation_id = aws_eip.nat.id
+    
+    subnet_id = aws_subnet.public_1.id
+
+    tags = {
+        Name = "${local.name_prefix}-nat"
+    }
+
+    depends_on = [aws_internet_gateway.main]
+}
